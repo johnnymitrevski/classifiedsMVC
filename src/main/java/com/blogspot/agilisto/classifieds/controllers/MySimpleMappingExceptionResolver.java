df465e34e1f158d8d2c09201d04150ccaf3dd072
johnny.mitrevski <johnny.mitrevski@gmail.com>
2013-04-05 23:57:48 +1100

    add custom exception handler

---
+++ src/main/java/com/blogspot/agilisto/classifieds/controllers/MySimpleMappingExceptionResolver.java
@@ -0,0 +1,7 @@
+package com.blogspot.agilisto.classifieds.controllers;
+
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+public class MySimpleMappingExceptionResolver extends SimpleMappingExceptionResolver{
+
+}
